716febc8d4a4afcfef5f2eb0eb6f63dc6ff46bdd
wangzihan <472704698@qq.com>
2015-03-17 19:25:40 -0700

    .

---
+++ log.h
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "fstream"
+#include "string"
+
+class log
+{
+private:
+    char * lfile;
+    std::ofstream ofile;
+
+public:
+    log():lfile("./server.log")
+    {
+        ofile.open(lfile);
+    }
+    
+    ~log()
+    {
+        ofile.close();
+    }
+
+    void common(char * c, std::string s)
+    {
+        ofile<<c<<s<<std::endl;
+    }
+};
